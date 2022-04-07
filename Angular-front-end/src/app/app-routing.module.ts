3852ba4a6a698d63ee244a989a5d006cc0bd8ec1
Randriambeloson <quickact.dev.mada@gmail.com>
2022-04-07 18:24:59 +0300

    init login - init signin - init loader

--- Angular-front-end/src/app/app-routing.module.ts
+++ Angular-front-end/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {LoaderComponent} from './loader/loader.component';
+import {SigninComponent} from './signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'loader', component: LoaderComponent },
+  { path: 'signin', component: SigninComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular-front-end/src/app/app.module.ts
+++ Angular-front-end/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LoaderComponent } from './loader/loader.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoaderComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
